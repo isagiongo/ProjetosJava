88329c6c3d6d771395682b3dea90fc680db3254a
Isa Giongo <isagiongo@yahoo.com.br>
2016-07-18 22:25:37 -0300

    Variáveis

---
+++ AulasBasicas/Variaveis/src/variaveis/Variaveis.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package variaveis;
+
+/**
+ *
+ * @author Isa Gb
+ */
+public class Variaveis {
+
+    public static void main(String[] args) {
+        
+        int idade = 10;
+        String nome = "Isadora";
+        
+        idade = 36;
+        
+        System.out.println("A idade de "+nome+" é " + idade);
+    
+    }
+    
+}
